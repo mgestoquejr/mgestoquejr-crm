f3444b6588cc1d5c09f0b1186831f64a8dd1c868
Omar Estoque <mgestoquejr@gmail.com>
2024-11-30 15:04:58 -0600

    Group customers by stage

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,8 @@ class Customer < ApplicationRecord
   validates :probability, numericality: { greater_than_or_equal_to: 0 }
 
   enum :stage, closed: "closed", contacted: "contacted", diligence: "diligence", lead: "lead", rejected: "rejected"
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
